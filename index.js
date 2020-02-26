a19d007168e2d34793ad0f19b056911ec95cad29
Kelleymb <kelleybuccieri@gmail.com>
2020-02-26 14:55:34 -0500

    final commit

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ $(function() {
         event.preventDefault();
     
 
-/* add item to the <li> to the shopping list <ul> */
-const addItem = $('event.currentTarget').find("#shopping-list-entry");
-$('ul').append(' <li><span class="shopping-item">${addItem.val()}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button>')
-
-
-})
+        /* add item to the <li> to the shopping list <ul> */
+        const addItem = $(event.currentTarget).find("#shopping-list-entry");
+        $('ul').append(`<li><span class="shopping-item">${addItem.val()}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button>`)
+    });
+/* can insert lines 15-17 && lines 29-31 here for simpler functionality*/
 });
 
 /* add a strikethrough with class .shopping-item__checked */
 $(function() {
     $('ul').on('click', '.shopping-item-toggle', function(){
         $(this).closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
-    })
+    });
 });
 
 
-/* const checkItem = $('ul').on('click', '.shopping-item-toggle', function(){
+/* REMOVED const checkItem = $('ul').on('click', '.shopping-item-toggle', function(){
     $(this).find('.shopping-item').toggleClass("shopping-item__checked");
 });  */
 
@@ -29,11 +28,11 @@ $(function() {
 $(function(){
     $('ul').on('click', '.shopping-item-delete', function(){
         $(this).closest('li').remove();
-    })
-})
+    });
+});
 
 
-/* const removeItem = $('ul').on('click', '.shopping-item-delete', function(){
+/* REMOVED const removeItem = $('ul').on('click', '.shopping-item-delete', function(){
     $(this).closest('li').remove();
 });
 */
